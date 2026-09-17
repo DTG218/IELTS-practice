ff345f7ca7a0902a01416c64d694d3c76040cb06
LuHaotang <2024271036@mails.szu.edu.cn>
2026-09-17 12:39:03 +0000

    fix: use iPad-compatible reading host in resource resolver

--- js/core/resourceCore.js
+++ js/core/resourceCore.js
@@ -412,7 +412,7 @@
             params.set('examId', String(examId));
         }
         params.set('dataKey', String(resolvedDataKey));
-        return `assets/generated/reading-exams/reading-practice-unified.html?${params.toString()}`;
+        return `assets/generated/reading-exams/reading-practice-ipad.html?${params.toString()}`;
     }
 
     function resolveExamBasePath(exam) {
