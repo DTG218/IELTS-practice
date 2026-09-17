134d4fd838f555b963734e0c839b8ffe706d573d
LuHaotang <2024271036@mails.szu.edu.cn>
2026-09-17 12:38:35 +0000

    fix: route reading practice through iPad-compatible host

--- js/app/examSessionMixin.js
+++ js/app/examSessionMixin.js
@@ -237,8 +237,8 @@
             }
             const query = params.toString();
             const url = query
-                ? `assets/generated/reading-exams/reading-practice-unified.html?${query}`
-                : 'assets/generated/reading-exams/reading-practice-unified.html';
+                ? `assets/generated/reading-exams/reading-practice-ipad.html?${query}`
+                : 'assets/generated/reading-exams/reading-practice-ipad.html';
             return typeof this._ensureAbsoluteUrl === 'function'
                 ? this._ensureAbsoluteUrl(url)
                 : url;
